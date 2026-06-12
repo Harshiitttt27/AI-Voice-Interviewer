75a33fe360487a5036a874c946de3457dce49a35
harshit <harrysethi52@gmail.com>
2026-06-13 01:00:44 +0530

    main file updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -6,22 +7,33 @@ from app.api import auth
 from app.api import resume
 from app.api.mailtestingapi import router as mail_router
 
-app = FastAPI(title="Voice AI Interviewer")
 
+from app.core.database import Base, engine
 
+from app.models.user import User
+from app.models.interview import InterviewSession
+from app.models.question import Question
+from app.models.answer import Answer
+from app.models.evaluation import Evaluation
+
+app = FastAPI()
+
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
         "https://ai-voice-interviewer-two.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
+    print("Tables created on Neon DB")
 
 # routers
 app.include_router(auth.router)
